12dca683960fbd093dfd680fdbd2d66532609365
renzbagaporo <renz.bagaporo@gmail.com>
2025-01-05 16:40:20 +0900

    Cleanup unneeded functions

--- board/src/imxrt10xx/power.rs
+++
@@ -1,9 +0,0 @@
-use crate::{hal::dcdc, ral, RunMode};
-
-/// Set the target power for the provided `run_mode`.
-pub fn set_target_power(dcdc: &mut ral::dcdc::DCDC, run_mode: RunMode) {
-    let millivolts = match run_mode {
-        RunMode::Overdrive => 1250,
-    };
-    dcdc::set_target_vdd_soc(dcdc, millivolts);
-}
