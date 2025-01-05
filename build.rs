c3b7aa32e522c74add168d9d3f1934c5b82a36a2
renzbagaporo <renz.bagaporo@gmail.com>
2025-01-05 20:14:33 +0900

    Remove device linker script

--- build.rs
+++ build.rs
@@ -1,13 +1,8 @@
 //! This build script prepares the HAL build.
 
 use imxrt_rt::{Family, Memory, RuntimeBuilder};
-use std::{env, fs, path::PathBuf};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let out_dir = env::var("OUT_DIR").map(PathBuf::from)?;
-    println!("cargo:rustc-link-search={}", out_dir.display());
-    fs::write(out_dir.join("device.x"), DEVICE_X)?;
-
     RuntimeBuilder::from_flexspi(Family::Imxrt1010, 16 * 1024 * 1024)
         .flexram_banks(imxrt_rt::FlexRamBanks {
             ocram: 1,
@@ -21,6 +16,3 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rustc-cfg=chip=\"imxrt1010\"");
     return Ok(());
 }
-
-const DEVICE_X: &str = r#"
-"#;
